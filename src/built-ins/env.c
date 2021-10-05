6fea75b1bac5a401ae7648dd47441d05bb79fc62
mida <mida@student.42.fr>
2021-10-05 16:43:40 +0200

    env

--- src/built-ins/env.c
+++ src/built-ins/env.c
@@ -1,6 +1,6 @@
 #include "../../include/minishell.h"
 
-void    print_env_var(t_command *cmd)
+/*void    print_env_var(t_command *cmd)
 {
     t_env *env_list;
 
@@ -15,4 +15,18 @@ void    print_list(t_env *head)
         printf("%s\n", head->data);
         head = head->next;
     }
+}*/
+
+int	my_env(t_list *env)
+{
+	t_list	*tmp;
+
+	tmp = env;
+	while (tmp != NULL)
+	{
+		write(1, tmp->content, ft_strlen(tmp->content));
+		write(1, "\n", 1);
+		tmp = tmp->next;
+	}
+	return (0);
 }
