d8ff95a558f7650a8e72235237ee3dc318442b94
mida <mida@student.42.fr>
2021-10-05 02:41:30 +0200

    pwd

--- src/built-ins/pwd.c
+++ src/built-ins/pwd.c
@@ -1,6 +1,6 @@
 #include "../../include/minishell.h"
 
-char    *print_working_directory(t_command *cmd)
+/*char    *print_working_directory(t_command *cmd)
 {
     t_env   *tmp;
     char    *path;
@@ -17,4 +17,23 @@ char    *print_working_directory(t_command *cmd)
         tmp = tmp->next;
     }
     return (path);
+}*/
+
+int	my_pwd(void)
+{
+	char	*abs_path;
+	char	*res;
+
+	abs_path = NULL;
+	res = getcwd(NULL, 0);
+	if (res == NULL)
+	{
+		printf("%s\n", strerror(errno));
+		return (-1);
+	}
+	write(1, res, ft_strlen(res));
+	write(1, "\n", 1);
+	free(res);
+	return (1);
 }
+
